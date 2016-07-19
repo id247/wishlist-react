e9f7d7973b9ef9d02d1bab63740b134f2ea83da9
simplecss <simplecss@yandex.ru>
2016-07-19 18:06:57 +0300

    profile reducer deleted

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 
-import { profile } from './profile';
 import { loading } from './loading';
 import { xml } from './xml';
 import { menu } from './menu';
 import { wishlist } from './wishlist';
 
 const rootReducer = combineReducers({
-	profile,
 	loading,
 	xml,
 	menu,
